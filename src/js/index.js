0c9a1d5a1fdb4e399516afe0701002bebffd2c8d
Dissmay <Dissmay21@gmail.com>
2019-08-15 20:34:31 +0300

    остался последний блок

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+$(function(){
+
+
+ $('.slider-for').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  fade: true,
+  asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  asNavFor: '.slider-for',
+  dots: true,
+  centerMode: true,
+  focusOnSelect: true,
+  prevArrow: $('.slider-container__btn_prev'),
+    nextArrow: $('.slider-container__btn_next')
+});
+
+});
